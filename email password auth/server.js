d807e33a41f9b151f6cb6cd1c9668ab92c38e770
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-01-14 13:39:31 +0530

    authroization check added

--- email password auth/server.js
+++ email password auth/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import session from "express-session";
-import bcrypt from "bcrypt";
 import crypto from "crypto";
+import cookieParser from "cookie-parser";
 
 // Initialise express application
 const app = express();
@@ -13,6 +13,7 @@ const __dirname = dirname(__filename);
 app.use(express.static(__dirname + "/client/"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use(
   session({
@@ -23,13 +24,18 @@ app.use(
 );
 
 // database for users account
-const USERS = {
+const USERS_ACCOUNT = {
   vignesh: "password1",
   kanna: "password2",
 };
 
-const sessions = new Map();
-const saltRounds = 10;
+const USER_SESSIONS = new Map();
+
+const USERS_PERMISSION = {
+  vignesh: "admin",
+  kanna: "read",
+};
+
 // starting express in port
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
@@ -44,11 +50,11 @@ app.get("/", (req, res) => {
 app.post("/login", (req, res) => {
   const password = req.body.password;
   const username = req.body.username;
-  const validAuth = USERS[username];
+  const validAuth = USERS_ACCOUNT[username];
   if (validAuth) {
     if (password === validAuth) {
       const sessionId = crypto.randomUUID();
-      sessions.set(sessionId, username);
+      USER_SESSIONS.set(sessionId, username);
       res
         .cookie("authSessionId", sessionId, {
           secure: true,
@@ -66,4 +72,19 @@ app.post("/login", (req, res) => {
 
 app.get("/homepage", (req, res) => {
   res.sendFile(__dirname + "/client/homepage.html");
+});
+
+// Homepage APIs
+app.post("/check-admin", (req, res) => {
+  const sessionId = req.cookies.authSessionId;
+  const currentUser = USER_SESSIONS.get(sessionId);
+  if (currentUser === undefined) {
+    res.redirect("/");
+  } else {
+    if (USERS_PERMISSION[currentUser] === "admin") {
+      res.send("you are authorized to use this button");
+    } else {
+      res.send("you are not authorized to use this button");
+    }
+  }
 });
